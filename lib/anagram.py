573eb67d6e93b7551a87a95d4ecf47fd1f5fabe5
Ziru Zou <jiyucoding@yahoo.com>
2023-02-20 06:16:54 -0500

    create match instance method, with word_list as arg, return the word that matches the reversed, otherwise return []

--- lib/anagram.py
+++ lib/anagram.py
@@ -3,3 +3,13 @@ class Anagram:
 
     def __init__(self, word):
         self.word = word
+
+    
+    def match(self, word_list):
+        self.word_list = word_list
+        reversed = self.word[::-1]
+        for word in self.word_list:
+            if word == reversed:
+                return word
+            else:
+                return []
